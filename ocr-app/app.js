52e9e9670862aa4afea2111c77d027e8c5b0d86b
SomeGuyWithLaptop <mjdtrrs@gmail.com>
2023-11-03 22:30:24 +0800

    removed dataframe code

--- ocr-app/app.js
+++ ocr-app/app.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const TextSegmentation = require('text-segmentation');
 
-
 // Remember to change the file name on imageFilePath
 const imageFilePath = './AnnexG_PyschEval_Written_page-0001.jpg'; 
 const outputDir = 'output'; // Directory to save OCR results
@@ -39,23 +38,5 @@ Tesseract.recognize(
     // Save the segmented text to a file
     fs.writeFileSync(segmentedTextFilePath, segmentedText);
     console.log('Segmented text saved to ' + segmentedTextFilePath);
-
-    // Split the segmented text into lines
-    const lines = segmentedText.split('\n');
-
-    // Create an array to hold the data for each processed document
-    const documents = [];
-
-    // Iterate through the lines and extract the fields (Name, Address, Allergies, etc.)
-    for (const line of lines) {
-      const [name, address, allergies, etc] = line.split('|').map((field) => field.trim());
-      documents.push({ Name: name, Address: address, Allergies: allergies, Etc: etc });
-    }
-
- // Create a data frame from the documents array
-const df = new TextSegmentation.DataFrame(documents);
-
-    // Now you have a data frame with the structured data
-    console.log(df.toString());
   })
   .catch((error) => console.error(error));
